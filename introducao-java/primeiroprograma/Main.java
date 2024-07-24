216ae901831fa1d3e3666c1485f5b45a69fc41e4
Vinicius Gomes <viniciusgomes2202@gmail.com>
2024-07-24 16:08:23 -0300

    Add files via upload

---
+++ introducao-java/primeiroprograma/Main.java
@@ -0,0 +1,9 @@
+package primeiroprograma;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+        System.out.println("Primeiro programa em java - CursoEmVideo.");
+        System.out.println("vsm = public static void main(String[] args {})");
+        System.out.println("sout = System.out.println");
+    }
+}
